45f84661a91d3b722603020b798f4fe3320163a7
虞晨 <chen.yu@cicc.com.cn>
2025-08-05 10:01:49 +0800

    init

--- ckeditor-test/webpack.config.js
+++ ckeditor-test/webpack.config.js
@@ -1,16 +1,48 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
+  mode: "development",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /.(ts)|(tsx)$/,
+        exclude: /node_modules/,
+        use: "ts-loader",
+        options: {
+          transpileOnly: true, // 只转译，不做类型检查（由插件负责）
+        },
+      },
+      {
+        test: /.(js)|(jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /.css$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html'
-    })
+      template: "./src/index.html",
+    }),
   ],
-  devtool: 'source-map' // 启用源码映射
+  resolve: {
+    alias: {
+      ckeditor5: path.resolve(__dirname, "../ckeditor5-master"),
+      "@ckeditor/ckeditor5-build-classic": path.resolve(
+        __dirname,
+        "../ckeditor5-master/packages/ckeditor5-editor-classic/src/index.js"
+      ),
+    },
+  },
+  devtool: "source-map", // 启用源码映射
 };
